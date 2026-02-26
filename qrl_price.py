ce91ab5e4e2a1fc25d7be22a02c04d3646282fad
ghazouani132 <40453979+ghazouani132@users.noreply.github.com>
2026-02-26 22:28:30 +0100

    Update qrl_price.py

--- qrl_price.py
+++ qrl_price.py
@@ -1,30 +1,36 @@
-import os
+
 import requests
 import time
+from datetime import datetime
 
-TELEGRAM_TOKEN = "8396116673:AAEP8q3ZClcSdU3PZXQaOp2WR34JQS3wytc"
-TELEGRAM_CHAT_ID = "-1003732439601" CHECK_EVERY = 900 # 15 دقيقة
-def send_telegram(msg): url = f"https://api.telegram.org/bot8396116673:AAEP8q3ZClcSdU3PZXQaOp2WR34JQS3wytc/sendMessage" 
+TELEGRAM_TOKEN   = "8396116673:AAEP8q3ZClcSdU3PZXQaOp2WR34JQS3wytc"
+TELEGRAM_CHAT_ID = "-1003732439601"
+CHECK_EVERY      = 900  # 15 دقيقة
+
+def send_telegram(msg):
+    url = f"https://api.telegram.org/bot8396116673:AAEP8q3ZClcSdU3PZXQaOp2WR34JQS3wytc/sendMessage"
     requests.post(url, data={"chat_id": TELEGRAM_CHAT_ID, "text": msg, "parse_mode": "HTML"})
 
 def get_qrl_price():
-    url = "https://api.coingecko.com/api/v3/simple/price?ids=the-quantum-resistant-ledger&vs_currencies=usd&include_24hr_change=true"
-    r = requests.get(url, timeout=20).json()
-    price = r["the-quantum-resistant-ledger"]["usd"]
-    change = r["the-quantum-resistant-ledger"]["usd_24h_change"]
-    return price, change
+    try:
+        url = "https://api.coingecko.com/api/v3/simple/price?ids=quantum-resistant-ledger&vs_currencies=usd&include_24hr_change=true"
+        r   = requests.get(url).json()
+        price  = r["quantum-resistant-ledger"]["usd"]
+        change = r["quantum-resistant-ledger"]["usd_24h_change"]
+        return price, change
+    except Exception as e:
+        print(f"Error: {e}")
+        return None, None
 
 def run():
+    print("Bot Started")
     while True:
-        try:
-            price, change = get_qrl_price()
-            msg = f"{price:.4f}\n{change:.2f}%"
+        price, change = get_qrl_price()
+        if price is not None:
+            msg = f"${price:.4f}\n{change:.2f}%"
             send_telegram(msg)
-        except Exception as e:
-            print("Error:", e)
+            print("Sent:", msg)
         time.sleep(CHECK_EVERY)
 
 if __name__ == "__main__":
     run()
-
-
