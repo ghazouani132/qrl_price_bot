01db7d24881a6ce296f23e8b5e57e02dba770ef2
ghazouani132 <40453979+ghazouani132@users.noreply.github.com>
2026-02-28 12:58:51 +0100

    Update qrl_price.py

--- qrl_price.py
+++ qrl_price.py
@@ -1,15 +1,14 @@
 import requests
 import time
-import os
 
-TELEGRAM_TOKEN   = os.getenv("TELEGRAM_TOKEN")
-TELEGRAM_CHAT_ID = os.getenv("-1003732439601")
-CHECK_EVERY      = 900  # 15 minutes
+TELEGRAM_TOKEN   = "TELEGRAM_TOKEN"
+TELEGRAM_CHAT_ID = -1003732439601
+CHECK_EVERY      = 900
 
 def send_telegram(msg):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
     requests.post(url, data={
-        "chat_id": -1003732439601,
+        "chat_id": TELEGRAM_CHAT_ID,
         "text": msg,
         "parse_mode": "HTML"
     })
@@ -17,7 +16,7 @@ def send_telegram(msg):
 def get_qrl_price():
     try:
         url = "https://api.coingecko.com/api/v3/simple/price?ids=quantum-resistant-ledger&vs_currencies=usd"
-        r = requests.get(url).json()
+        r     = requests.get(url).json()
         price = r["quantum-resistant-ledger"]["usd"]
         return price
     except Exception as e:
@@ -27,35 +26,24 @@ def get_qrl_price():
 def run():
     print("Bot Started")
     previous_price = None
-
     while True:
         price = get_qrl_price()
-
         if price is not None:
             if previous_price is None:
-                # أول تشغيل
-                arrow = "→"
+                arrow          = "→"
                 percent_change = 0.0
             else:
                 percent_change = ((price - previous_price) / previous_price) * 100
-
                 if percent_change > 0:
                     arrow = "🟢 ↑"
                 elif percent_change < 0:
                     arrow = "🔴 ↓"
                 else:
                     arrow = "→"
-
-            msg = f"QRL: ${price:.4f}\n{arrow} {percent_change:.2f}% (15m)"
             send_telegram(msg)
             print("Sent:", msg)
-
             previous_price = price
-
         time.sleep(CHECK_EVERY)
 
 if __name__ == "__main__":
     run()
-
-
-
